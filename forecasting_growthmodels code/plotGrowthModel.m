f1f682086ffb0257639f0fc85bc36208d50da84a
gchowell <31742415+gchowell@users.noreply.github.com>
2023-08-22 09:42:58 -0400

    Add files via upload

--- forecasting_growthmodels code/plotGrowthModel.m
+++ forecasting_growthmodels code/plotGrowthModel.m
@@ -1,59 +1,11 @@
-function plotGrowthModel(flag1_pass,params0_pass,I0_pass,windowsize1_pass)
+function [curve,params]=plotGrowthModel(flag1_pass,r_pass,p_pass,a_pass,K_pass,I0_pass,windowsize1_pass)
 
 % <============================================================================>
 % < Author: Gerardo Chowell  ==================================================>
 % <============================================================================>
 
-% Fitting and forecasting model to epidemic data with quantified uncertainty
-
-close all
-
-% <============================================================================>
-% <=================== Declare global variables ===============================>
-% <============================================================================>
-
-global method1 % Parameter estimation method
-
-% <============================================================================>
-% <=================== Load parameter values supplied by user =================>
-% <============================================================================>
-
-[cadfilename1_INP,caddisease_INP,datatype_INP, dist1_INP, numstartpoints_INP,M_INP,flag1_INP,model_name1_INP,fixI0_INP,windowsize1_INP,tstart1_INP,tend1_INP]=options_fit;
-
-% <============================================================================>
-% <================================ Datasets properties ==============================>
-% <============================================================================>
-
-cadfilename1=cadfilename1_INP;
-
-DT=1;
-
-caddisease=caddisease_INP;
-
-datatype=datatype_INP;
-
-% <=============================================================================>
-% <=========================== Parameter estimation ============================>
-% <=============================================================================>
-
-%method1=0; % Type of estimation method: 0 = LSQ
-
-d=1;
-
-dist1=dist1_INP; %Define dist1 which is the type of error structure:
-
-% LSQ=0,
-% MLE Poisson=1,
-% Pearson chi-squard=2,
-% MLE (Neg Binomial)=3, with VAR=mean+alpha*mean;
-% MLE (Neg Binomial)=4, with VAR=mean+alpha*mean^2;
-% MLE (Neg Binomial)=5, with VAR=mean+alpha*mean^d;
-
-
-numstartpoints=numstartpoints_INP; % Number of initial guesses for optimization procedure using MultiStart
-
-M=M_INP; % number of bootstrap realizations to characterize parameter uncertainty
-
+% Plot growth model solution using the parameter values supplied by the
+% user
 
 % <==============================================================================>
 % <============================== Growth model =====================================>
@@ -65,72 +17,226 @@ GRM=2;  % 2 = GRM
 LM=3;   % 3 = LM
 RICH=4; % 4 = Richards
 
-model_name1=model_name1_INP;
-
-% <==================================================================================>
-% <=============================== other parameters=======================================>
-% <==================================================================================>
-
-fixI0=fixI0_INP; % 0=Estimate the initial number of cases; 1 = Fix the initial number of cases according to the first data point in the time series
-
-% <==============================================================================>
-% <======================== Load epidemic data ========================================>
-% <==============================================================================>
-
-% data=load(strcat('./input/',cadfilename1,'.txt'));
-% 
-% if strcmp('CUMULATIVE',upper(cadfilename1(1:10)))==1
-% 
-%     data(:,2)=[data(1,2);diff(data(:,2))]; % Incidence curve
-% 
-% end
-
-% <==============================================================================>
-% <========================== Forecasting parameters ===================================>
-% <==============================================================================>
-
-getperformance=1; % flag or indicator variable (1/0) to calculate forecasting performance or not
-
-forecastingperiod=0; %forecast horizon (number of data points ahead)
-
-% <==================================================================================>
-% <========================== Parameters of the rolling window analysis =========================>
-% <==================================================================================>
-
 if exist('flag1_pass','var')==1 & isempty(flag1_pass)==0
     flag1=flag1_pass;
 else
-    flag1=flag1_INP;
+    'flag1 value is required'
+    return
 end
 
-params0=params0_pass;
+% <==================================================================================>
+% <========================== Parameter values ======================================>
+% <==================================================================================>
 
-I0=I0_pass;
+a1=1;
+p1=1;
+K1=1;
 
-if exist('windowsize1_pass','var')==1 & isempty(windowsize1_pass)==0
+switch flag1
+
+    case 0
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the GGM model'
+            return
+        end
+
+        if exist('p_pass','var')==1 & isempty(p_pass)==0
+            p1=p_pass;
+        else
+            'p value is required for the GGM model'
+            return
+
+        end
+
+        model_name1='Generalized Growth Model';
+
+        params=[r1 p1];
+
+    case 1
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the GLM model'
+            return
+        end
+
+        if exist('p_pass','var')==1 & isempty(p_pass)==0
+            p1=p_pass;
+        else
+            'p value is required for the GLM model'
+            return
+        end
+
+        if exist('K_pass','var')==1 & isempty(K_pass)==0
+            K1=K_pass;
+        else
+            'K value is required for the GLM model'
+            return
+
+        end
+
+        model_name1='Generalized Logistic Growth Model';
+        
+        params=[r1 p1 K1];
+
+    case 2
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the Generalized Richards model'
+            return
+        end
+
+        if exist('p_pass','var')==1 & isempty(p_pass)==0
+            p1=p_pass;
+        else
+            'p value is required for the GLM model'
+            return
+        end
+
+        if exist('a_pass','var')==1 & isempty(a_pass)==0
+            a1=a_pass;
+        else
+            'a value is required for the Richards model'
+            return
+        end
+
+        if exist('K_pass','var')==1 & isempty(K_pass)==0
+            K1=K_pass;
+        else
+            'K value is required for the Generalized Richards model'
+            return
+        end
+
+        model_name1='Generalized Richards Model';
+
+        params=[r1 p1 a1 K1];
+
+
+    case 3
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the LM model'
+            return
+        end
+
+        if exist('K_pass','var')==1 & isempty(K_pass)==0
+            K1=K_pass;
+        else
+            'K value is required for the LM model'
+            return
+
+        end
+
+        model_name1='Logistic Growth Model';
+
+         params=[r1 K1];
+
+    case 4
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the Richards model'
+            return
+        end
+
+        if exist('a_pass','var')==1 & isempty(a_pass)==0
+            a1=a_pass;
+        else
+            'a value is required for the Richards model'
+            return
+        end
+
+        if exist('K_pass','var')==1 & isempty(K_pass)==0
+            K1=K_pass;
+        else
+            'K value is required for the Richards model'
+            return
+
+        end
+
+      model_name1='Richards Model';
+
+      params=[r1 a1 K1];
+
+    case 5 % Gompertz
+
+        if exist('r_pass','var')==1 & isempty(r_pass)==0
+            r1=r_pass;
+        else
+            'r value is required for the Richards model'
+            return
+        end
+
+        if exist('a_pass','var')==1 & isempty(a_pass)==0
+            a1=a_pass;
+        else
+            'a value is required for the Richards model'
+            return
+        end
+
+        if exist('K_pass','var')==1 & isempty(K_pass)==0
+            K1=K_pass;
+        else
+            'K value is required for the Richards model'
+            return
+
+        end
+
+      model_name1='Gompertz Model';
+
+      params=[r1 a1 K1];
+
+
+end
+
+% <==================================================================================>
+% <========================== Initial condition C(0) ================================>
+% <==================================================================================>
+
+if exist('I0_pass','var')==1 & isempty(I0_pass)==0
+    I0=I0_pass;
+else
+    'Initial value C(0) is required'
+    return
+end
+
+% <==================================================================================>
+% <========================== Time span =============================================>
+% <==================================================================================>
+
+if exist('windowsize1_pass','var')==1 & isempty('windowsize1_pass')==0
     windowsize1=windowsize1_pass;
 else
-    windowsize1=windowsize1_INP;
+    'Simulation duration is required'
+    return
 end
-
-%params0=initialParams(data(:,2),flag1);
-
-r=params0(1);
-p=params0(2);
-a=params0(3);
-K=params0(4);
 
 timevect=0:1:windowsize1;
 
 options=optimoptions('fmincon','Algorithm','sqp','StepTolerance',1.0000e-6,'MaxFunEvals',20000,'MaxIter',20000);
 
-[~,F]=ode15s(@modifiedLogisticGrowth,timevect,I0,options,r,p,a,K,flag1);
+[~,F]=ode15s(@modifiedLogisticGrowth,timevect,I0,options,r1,p1,a1,K1,flag1);
 
-fitcurve=abs([F(1,1);diff(F(:,1))]);
+curve=abs([F(1,1);diff(F(:,1))]);
 
-plot(timevect,fitcurve,'b-');
+line1=plot(timevect,curve,'b-');
+set(line1,'LineWidth',2)
 
+xlabel('Time')
 
+ylabel('dC(t)/dt')
 
+title(model_name1)
 
+set(gca,'FontSize', 24);
+set(gcf,'color','white')
 
